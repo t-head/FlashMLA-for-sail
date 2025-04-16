afa399f72bb8cad53ea17ddf43e77bdf91d38cca
chunjiang.hc <chunjiang.hc@alibaba-inc.com>
2025-04-16 17:44:09 +0800

    FlashMLA: remove date from version number
    When installing FlashMLA with pip, it is necessary to specify the version number. If the version number contains a date, it can make it difficult to determine the correct version when using pip.
    
    to #64865815: flashmla
    
    Test Case: non test case
    
    Reviewed By: Wu Tao, Wu Zhiwen
    
    Change-Id: I2795185078cde24cd988c79e235f87691b7d8ad5

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ except Exception as _:
 
 setup(
     name="flash_mla",
-    version="1.0.0" + rev,
+    version="1.0.0",
     packages=find_packages(include=['flash_mla']),
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
